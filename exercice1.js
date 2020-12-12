3e8e38090fae8c85b07721a44eebec2d172451f2
Nux007 <007.nux@gmail.com>
2020-12-12 12:18:55 +0100

    Exo 87

---
+++ exercice1.js
@@ -0,0 +1,15 @@
+let voiture = {
+    "Nom" : "Eggo",
+    "imgUrl" : "https://images.caradisiac.com/logos/9/9/8/2/159982/S7-Citroen-Eggo-concept-de-citadine-du-futur-73689.jpg",
+    "nombresRoues" : 4,
+    "Couleur" : "Marron",
+    "Contructeur" : "Citroen",
+    "Carburant" : "Solaire",
+    "nombrePortes" : 3,
+    "Autonomie" : "450km",
+    "vitesseMaxi" : "120km/h"
+};
+
+
+
+
